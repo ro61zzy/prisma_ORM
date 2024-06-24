d6a0759b790ed6971af9e61482dd435f5be5cbc6
rozzy <rosekaremeri@gmail.com>
2024-06-24 16:29:29 +0300

    additional comments

--- script.ts
+++ script.ts
@@ -46,8 +46,10 @@ async function main() {
 
 
 //Prisma Client only returns scalar fields in the result objects of a query. 
+//even though you also created a new Post record for the new User record, the console only printed an object with three scalar fields: id, email an
+//{ id: 2, email: 'bob@prisma.io', name: 'Bob' }
 
-
+//In order to also retrieve the Post records that belong to a User, you can use the include option via the posts relation field
 
   const usersWithPosts = await prisma.user.findMany({
     include: {
